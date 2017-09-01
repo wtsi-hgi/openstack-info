2845f31f21eca8b8b78ba948aad7a15bfc8a8a21
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-09-01 18:31:20 +0100

    Adds fix for tests in Travis CI.

--- openstackinfo/tests/_common.py
+++ openstackinfo/tests/_common.py
@@ -6,7 +6,7 @@ TEST_PASSWORD = "pass"
 TEST_AUTH_URL = "http://example.com:5000/v2.0/"
 TEST_TENANT_NAME = "tenant"
 
-RESOURCES_PATH = "resources"
+RESOURCES_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "resources")
 
 with open(os.path.join(RESOURCES_PATH, "information-indexed-by-type.json"), "r") as file:
     INFORMATION_INDEXED_BY_TYPE = json.load(file)
