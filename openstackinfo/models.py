9310bee5351b7c1daa9b44553e13414ae0e38b4a
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-09-01 18:49:25 +0100

    Fixes cyclic dependency issue.

--- openstackinfo/models.py
+++ openstackinfo/models.py
@@ -1,7 +1,4 @@
-from typing import NamedTuple, List, Dict, Type
-
-from openstackinfo.indexers import InformationIndexer, InformationIndexerByType
-from openstackinfo.retrievers import InformationRetriever
+from typing import NamedTuple, Type
 
 
 class Credentials(NamedTuple):
@@ -18,5 +15,8 @@ class RunConfiguration(NamedTuple):
     """
     Run configuration.
     """
+    from openstackinfo.indexers import InformationIndexer, InformationIndexerByType
+    from openstackinfo.retrievers import InformationRetriever
+
     retriever: InformationRetriever
     indexer: Type[InformationIndexer] = InformationIndexerByType
