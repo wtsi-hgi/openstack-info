78ae1a652b1fc97572479e273af5b59912ff2af6
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-09-06 10:30:39 +0100

    Bumps version to 5.1.1.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name="openstackinfo",
-    version="5.1.0",
+    version="5.1.1",
     packages=find_packages(exclude=["tests"]),
     install_requires=open("requirements.txt", "r").readlines(),
     url="https://github.com/wtsi-hgi/openstack-info",
