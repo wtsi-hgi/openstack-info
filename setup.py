d1836253e600e06bae663c577aa668d625c0b448
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-10-10 15:54:50 +0100

    Releases version 5.5.0.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name="openstackinfo",
-    version="5.4.0",
+    version="5.5.0",
     packages=find_packages(exclude=["tests"]),
     install_requires=open("requirements.txt", "r").readlines(),
     url="https://github.com/wtsi-hgi/openstack-info",
